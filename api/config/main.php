59fd4f8ef4803b844d219f16c49ee1363b6619c8
hts <hts@meitu.com>
2018-10-25 23:29:14 +0800

    config

--- api/config/main.php
+++ api/config/main.php
@@ -1,18 +1,18 @@
 <?php
 //框架内置的配置项 -- 适用当前应用所有环境
 $config = [
-    'id' => 'app-customer',
+    'id' => 'app-api',
     'basePath' => dirname(__DIR__),
-    'controllerNamespace' => 'customer\controllers',
+    'controllerNamespace' => 'api\controllers',
     'components' => [
         'request' => [
-            'csrfParam' => '_csrf-customer',
+            'csrfParam' => '_csrf-api',
             'cookieValidationKey' => 'j1m7zFSqH7bpTns_36yWiQHKLKEveR70',
             'enableCsrfValidation' => false,
         ],
         'session' => [
             // this is the name of the session cookie used for login on the customer
-            'name' => 'advanced-customer',
+            'name' => 'advanced-api',
         ],
         'errorHandler' => [
             'class' => 'common\components\ErrorHandler', //自定义错误处理类
